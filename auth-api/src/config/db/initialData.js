09eeb26da935d2f13a24f6795fb0bca8b37cd51d
wellington cruz <wellington.max1@gmail.com>
2022-07-04 14:39:34 -0300

    Adicionando arquivos de configurações.

---
+++ auth-api/src/config/db/initialData.js
@@ -0,0 +1,18 @@
+import bcrypt from "bcrypt";
+import User from "../../modules/user/model/User.js";
+
+export async function createInitialData() {
+    try {
+        await User.sync({ force: true });
+    
+        let password = await bcrypt.hash("123456", 10);
+    
+        await User.create({
+            name: "User Test",
+            email: "testuser@gmail.com",
+            password: password
+        });
+    } catch (error) {
+        console.error(error);
+    }
+}
