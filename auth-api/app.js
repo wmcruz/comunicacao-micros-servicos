d4f1dfbc2da32eca2d47583c9b09ec12b3e87367
wellington cruz <wellington.max1@gmail.com>
2022-07-04 14:32:16 -0300

    Criação de app principal de execução do auth-api

---
+++ auth-api/app.js
@@ -0,0 +1,24 @@
+import express from "express";
+import * as db from "./src/config/db/initialData.js";
+import UserRoutes from "./src/modules/user/routes/UserRoutes.js";
+
+const app = express();
+const env = process.env;
+const PORT = env.PORT || 8080;
+
+db.createInitialData();
+
+app.use(express.json());
+app.use(UserRoutes);
+
+app.get("/api/status", (req, res) => {
+    return res.status(200).json({
+        service: "Auth-API",
+        status: "up",
+        httpStatus: 200        
+    });
+});
+
+app.listen(PORT, () => {
+    console.info(`Server has started succefully at port ${PORT}`);
+});
