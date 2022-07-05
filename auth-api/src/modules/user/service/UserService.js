be333515e0c31f3cbbebca96d743882d63b0f646
wellington cruz <wellington.max1@gmail.com>
2022-07-05 20:46:11 -0300

    Validação de token de destino ao usuário.

--- auth-api/src/modules/user/service/UserService.js
+++ auth-api/src/modules/user/service/UserService.js
@@ -3,6 +3,7 @@ import jwt from "jsonwebtoken";
 
 import UserRepository from "../repository/UserRepository.js";
 import UserException from "../exception/UserException.js";
+
 import * as httpStatus from "../../../config/constants/httpStatus.js";
 import * as secrets from "../../../config/constants/secrets.js";
 
@@ -11,13 +12,13 @@ class UserService {
     async findByEmail(req) {
         try {
             const { email } = req.params;
+            const { authUser } = req;
             this.validateRequestEmail(email);
             
             let user = await UserRepository.findByEmail(email);
             this.validateUserNotFound(user);
 
-            if (!email) {
-            }
+            this.validateAuthenticateUser(user, authUser);
 
             return {
                 status: httpStatus.SUCCESS,
@@ -47,6 +48,12 @@ class UserService {
         }
     }
 
+    validateAuthenticateUser(user, authUser) {
+        if (!authUser || user.id !== authUser.id) {
+            throw new UserException(httpStatus.FORBIDDEN, "You cannot see this user data.");
+        }
+    }
+
     async getAccessToken(req) {
         try {
             const { email, password } = req.body;
@@ -57,7 +64,7 @@ class UserService {
             await this.validatePassword(password, user.password);
 
             const authUser = { id: user.id, name: user.name, email: user.email };
-            const accessToken = jwt.sign({ authUser }, secrets.API_SECRET ,{ expiresIn: "1d"});
+            const accessToken = jwt.sign({ authUser }, secrets.API_SECRET, { expiresIn: "1d"});
 
             return {
                 status: httpStatus.SUCCESS,
@@ -82,7 +89,6 @@ class UserService {
             throw new UserException(httpStatus.UNAUTHORIZED, "Password doens't match.")
         }
     }
-
 }
 
 export default new UserService();
