bf3bd38f307119c9831a37a494de4bbfcc52a9fd
wellington cruz <wellington.max1@gmail.com>
2022-07-05 13:57:51 -0300

    Realizando o import no check access point no app principal do auth-api.

--- auth-api/app.js
+++ auth-api/app.js
@@ -1,6 +1,9 @@
 import express from "express";
-import * as db from "./src/config/db/initialData.js";
+
 import UserRoutes from "./src/modules/user/routes/UserRoutes.js";
+import checkToken from "./src/config/auth/checkToken.js";
+
+import * as db from "./src/config/db/initialData.js";
 
 const app = express();
 const env = process.env;
@@ -10,6 +13,8 @@ db.createInitialData();
 
 app.use(express.json());
 app.use(UserRoutes);
+
+app.use(checkToken);
 
 app.get("/api/status", (req, res) => {
     return res.status(200).json({
