da80d83d9664712e00f7e33c2b462b17a30c23f2
wellington cruz <wellington.max1@gmail.com>
2022-07-08 19:51:15 -0300

    Correção de IDs iniciais no arquivo de import.

--- product-api/src/main/resources/import.sql
+++ product-api/src/main/resources/import.sql
@@ -1,12 +1,10 @@
-INSERT INTO category(id, description) VALUES(1, 'Comic Books');
-INSERT INTO category(id, description) VALUES(2, 'Movies');
-INSERT INTO category(id, description) VALUES(3, 'Books');
+INSERT INTO category(id, description) VALUES(1000, 'Comic Books');
+INSERT INTO category(id, description) VALUES(1001, 'Movies');
+INSERT INTO category(id, description) VALUES(1002, 'Books');
 
-INSERT INTO supplier(id, name) VALUES(1, 'Panini Comics');
-INSERT INTO supplier(id, name) VALUES(2, 'Amazon');
+INSERT INTO supplier(id, name) VALUES(1000, 'Panini Comics');
+INSERT INTO supplier(id, name) VALUES(1001, 'Amazon');
 
-INSERT INTO product(id, name, quantity_available, fk_supplier, fk_category) VALUES(1, 'Crise nas Infinitas Terras', 10, 1, 1);
-
-INSERT INTO product(id, name, quantity_available, fk_supplier, fk_category) VALUES(2, 'Interestelar', 5, 2, 2);
-
-INSERT INTO product(id, name, quantity_available, fk_supplier, fk_category) VALUES(3, 'Harry Potter e a Pedra Filosofal', 3, 2, 3);
+INSERT INTO product(id, name, quantity_available, fk_supplier, fk_category) VALUES(1000, 'Crise nas Infinitas Terras', 10, 1000, 1000);
+INSERT INTO product(id, name, quantity_available, fk_supplier, fk_category) VALUES(1001, 'Interestelar', 5, 1001, 1001);
+INSERT INTO product(id, name, quantity_available, fk_supplier, fk_category) VALUES(1002, 'Harry Potter e a Pedra Filosofal', 3, 1001, 1002);
