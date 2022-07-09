9a99ee2606f4a9508181f4871f2db0f281f07af2
wellington cruz <wellington.max1@gmail.com>
2022-07-08 22:13:23 -0300

    Adicionando nova coluna de quando o produto foi criado, arquivo de importação inicial.

--- product-api/src/main/resources/import.sql
+++ product-api/src/main/resources/import.sql
@@ -5,6 +5,6 @@ INSERT INTO category(id, description) VALUES(1002, 'Books');
 INSERT INTO supplier(id, name) VALUES(1000, 'Panini Comics');
 INSERT INTO supplier(id, name) VALUES(1001, 'Amazon');
 
-INSERT INTO product(id, name, quantity_available, fk_supplier, fk_category) VALUES(1000, 'Crise nas Infinitas Terras', 10, 1000, 1000);
-INSERT INTO product(id, name, quantity_available, fk_supplier, fk_category) VALUES(1001, 'Interestelar', 5, 1001, 1001);
-INSERT INTO product(id, name, quantity_available, fk_supplier, fk_category) VALUES(1002, 'Harry Potter e a Pedra Filosofal', 3, 1001, 1002);
+INSERT INTO product(id, name, quantity_available, created_at, fk_supplier, fk_category) VALUES(1000, 'Crise nas Infinitas Terras', 10, CURRENT_TIMESTAMP, 1000, 1000);
+INSERT INTO product(id, name, quantity_available, created_at, fk_supplier, fk_category) VALUES(1001, 'Interestelar', 5, CURRENT_TIMESTAMP, 1001, 1001);
+INSERT INTO product(id, name, quantity_available, created_at, fk_supplier, fk_category) VALUES(1002, 'Harry Potter e a Pedra Filosofal', 3, CURRENT_TIMESTAMP, 1001, 1002);
