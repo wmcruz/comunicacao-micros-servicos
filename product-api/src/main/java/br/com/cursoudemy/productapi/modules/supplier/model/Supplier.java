5327caa76f0a0c8c7f9c2579c1fc728b9a21ec4f
wellington cruz <wellington.max1@gmail.com>
2022-07-08 22:15:53 -0300

    Novo método para conversão de supplier request para supplier.

--- product-api/src/main/java/br/com/cursoudemy/productapi/modules/supplier/model/Supplier.java
+++ product-api/src/main/java/br/com/cursoudemy/productapi/modules/supplier/model/Supplier.java
@@ -1,8 +1,10 @@
 package br.com.cursoudemy.productapi.modules.supplier.model;
 
+import br.com.cursoudemy.productapi.modules.supplier.dto.SupplierRequest;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.beans.BeanUtils;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -21,6 +23,14 @@ public class Supplier {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Integer id;
+
     @Column(name = "NAME", nullable = false)
     private String name;
+
+    public static Supplier of(SupplierRequest request) {
+        var supplier = new Supplier();
+        BeanUtils.copyProperties(request, supplier);
+
+        return supplier;
+    }
 }
