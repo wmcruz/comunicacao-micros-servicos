9ca7fe5add7932ee0485039c840834f47aa9967b
wellington cruz <wellington.max1@gmail.com>
2022-07-08 23:10:24 -0300

    Novas pesquisas de category implementadas no repository, service e controller.

--- product-api/src/main/java/br/com/cursoudemy/productapi/modules/category/controller/CategoryController.java
+++ product-api/src/main/java/br/com/cursoudemy/productapi/modules/category/controller/CategoryController.java
@@ -1,13 +1,17 @@
 package br.com.cursoudemy.productapi.modules.category.controller;
 
-import br.com.cursoudemy.productapi.modules.category.dto.CategoryResponse;
 import br.com.cursoudemy.productapi.modules.category.dto.CategoryRequest;
+import br.com.cursoudemy.productapi.modules.category.dto.CategoryResponse;
 import br.com.cursoudemy.productapi.modules.category.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/category")
@@ -20,4 +24,21 @@ public class CategoryController {
     public CategoryResponse save(@RequestBody CategoryRequest request) {
         return this.categoryService.save(request);
     }
+
+    @GetMapping
+    public List<CategoryResponse> findAll() {
+        return this.categoryService.findAll();
+    }
+
+    @GetMapping("{id}")
+    public CategoryResponse findById(@PathVariable Integer id) {
+        return this.categoryService.findByIdResponse(id);
+    }
+
+    @GetMapping("/description/{description}")
+    public List<CategoryResponse> findByDescription(@PathVariable String description) {
+        return this.categoryService.findByDescription(description);
+    }
+
+
 }
--- product-api/src/main/java/br/com/cursoudemy/productapi/modules/category/service/CategoryService.java
+++ product-api/src/main/java/br/com/cursoudemy/productapi/modules/category/service/CategoryService.java
@@ -9,16 +9,42 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ObjectUtils;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 public class CategoryService {
 
     @Autowired
     private CategoryRepository categoryRepository;
 
+    public CategoryResponse findByIdResponse(Integer id) {
+        return CategoryResponse.of(this.findById(id));
+    }
+
     public Category findById(Integer id) {
-        return this.categoryRepository
-                .findById(id)
+        if (ObjectUtils.isEmpty(id))
+            throw new ValidationException("The Category ID not informed.");
+
+        return this.categoryRepository.findById(id)
                 .orElseThrow(() -> new ValidationException("There's no category for the given ID."));
+    }
+
+    public List<CategoryResponse> findAll() {
+        return this.categoryRepository.findAll()
+                .stream()
+                .map(CategoryResponse::of)
+                .collect(Collectors.toList());
+    }
+
+    public List<CategoryResponse> findByDescription(String description) {
+        if (ObjectUtils.isEmpty(description))
+            throw new ValidationException("The category description must be informed.");
+
+        return this.categoryRepository.findByDescriptionIgnoreCaseContaining(description)
+                .stream()
+                .map(CategoryResponse::of)
+                .collect(Collectors.toList());
     }
 
     public CategoryResponse save(CategoryRequest request) {
@@ -29,8 +55,7 @@ public class CategoryService {
     }
 
     private void validateCategoryNameInformed(CategoryRequest request) {
-        if (ObjectUtils.isEmpty(request.getDescription())) {
+        if (ObjectUtils.isEmpty(request.getDescription()))
             throw new ValidationException("The category description was not informed.");
-        }
     }
 }
