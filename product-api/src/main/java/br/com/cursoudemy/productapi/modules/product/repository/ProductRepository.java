3dd3988ed57b06082818360c755bd498c87f5855
wellington cruz <wellington.max1@gmail.com>
2022-07-19 22:30:18 -0300

    Adicionando pesquisa se existe categoria ou fornecedor no produto.

--- product-api/src/main/java/br/com/cursoudemy/productapi/modules/product/repository/ProductRepository.java
+++ product-api/src/main/java/br/com/cursoudemy/productapi/modules/product/repository/ProductRepository.java
@@ -12,4 +12,7 @@ public interface ProductRepository extends JpaRepository<Product, Integer> {
     List<Product> findByCategoryId(Integer id);
 
     List<Product> findBySupplierId(Integer id);
+
+    Boolean existsByCategoryId(Integer id);
+    Boolean existsBySupplierId(Integer id);
 }
