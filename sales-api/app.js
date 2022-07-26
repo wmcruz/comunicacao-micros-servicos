22f69769110119a6129590d5ea67d5718280c601
wellington cruz <wellington.max1@gmail.com>
2022-07-26 20:59:00 -0300

    Adicionando dependencias e lógica para conexão com o mongodb.

--- sales-api/app.js
+++ sales-api/app.js
@@ -1,8 +1,11 @@
 import express from 'express';
+import { connect } from './src/config/db/mongoDbConfig.js';
 
 const app = express();
 const env = process.env;
 const PORT = env.PORT || 8082;
+
+connect();
 
 app.listen(PORT, () => {
     console.info(`Server started successfully at port ${PORT}`)
