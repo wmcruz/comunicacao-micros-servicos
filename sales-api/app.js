aec51e7cbd8d3009b1e064d82e4cb3300b85f09e
wellington cruz <wellington.max1@gmail.com>
2022-07-30 19:28:42 -0300

    Adicionando teste para serviço novo de publicação de produto estoque.

--- sales-api/app.js
+++ sales-api/app.js
@@ -6,6 +6,8 @@ import { connectRabbitMq } from './src/config/rabbitmq/rabbitConfig.js';
 
 import checkToken from './src/config/auth/checkToken.js';
 
+import { sendMessageToProductStockUpdateQueue } from './src/modules/product/rabbitmq/productStockUpdateSender.js';
+
 const app = express();
 const env = process.env;
 const PORT = env.PORT || 8082;
@@ -13,6 +15,29 @@ const PORT = env.PORT || 8082;
 connectMongoDb();
 connectRabbitMq();
 createInitialData();
+
+app.get("/teste", (req, res) => {
+    try {
+        sendMessageToProductStockUpdateQueue([
+            {
+                productId: 1000,
+                quantity: 3
+            },
+            {
+                productId: 1001,
+                quantity: 2
+            },
+            {
+                productId: 1002,
+                quantity: 1
+            }
+        ]);
+        return res.status(200).json({ status: 200 });
+    } catch (err) {
+        console.error(err);
+        return res.status(500).json({ error: true })
+    }
+});
 
 app.get('/api/status', (req, res) => {
     return res.status(200).json({
