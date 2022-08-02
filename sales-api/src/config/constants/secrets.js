9541d40d27a4c800e0774bf52b24bd0fb390b3e3
wellington cruz <wellington.max1@gmail.com>
2022-08-02 14:33:49 -0300

    Chamadas a API de produto.

--- sales-api/src/config/constants/secrets.js
+++ sales-api/src/config/constants/secrets.js
@@ -5,3 +5,5 @@ export const MONGO_DB_URL = env.MONGO_DB_URL ? env.MONGO_DB_URL : "mongodb://adm
 export const API_SECRET = env.API_SECRET ? env.API_SECRET : "YXV0aC1hcGktc2VjcmV0LWRldi0xMjM0NTYK";
 
 export const RABBIT_MQ_URL = env.RABBIT_MQ_URL ? env.RABBIT_MQ_URL : "amqp://localhost:5672";
+
+export const PRODUCT_API_URL = env.PRODUCT_API_URL ? env.PRODUCT_API_URL : "http://localhost:8081/api/product"
