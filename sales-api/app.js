a39ce493636c52d2dc6e1c5d4fa3e61807ad00fe
wellington cruz <wellington.max1@gmail.com>
2022-08-02 14:35:22 -0300

    Adicionando implementação do ecosistema de criação de alteração de uma ordem de venda.

--- sales-api/app.js
+++ sales-api/app.js
@@ -3,10 +3,9 @@ import express from 'express';
 import { connectMongoDb } from './src/config/db/mongoDbConfig.js';
 import { createInitialData } from "./src/config/db/initialData.js";
 import { connectRabbitMq } from './src/config/rabbitmq/rabbitConfig.js';
+import checkToken  from './src/config/auth/checkToken.js';
 
-import checkToken from './src/config/auth/checkToken.js';
-
-import { sendMessageToProductStockUpdateQueue } from './src/modules/product/rabbitmq/productStockUpdateSender.js';
+import orderRoutes from "./src/modules/sales/routes/OrderRoutes.js";
 
 const app = express();
 const env = process.env;
@@ -16,29 +15,6 @@ connectMongoDb();
 connectRabbitMq();
 createInitialData();
 
-app.get("/teste", (req, res) => {
-    try {
-        sendMessageToProductStockUpdateQueue([
-            {
-                productId: 1000,
-                quantity: 3
-            },
-            {
-                productId: 1001,
-                quantity: 2
-            },
-            {
-                productId: 1002,
-                quantity: 1
-            }
-        ]);
-        return res.status(200).json({ status: 200 });
-    } catch (err) {
-        console.error(err);
-        return res.status(500).json({ error: true })
-    }
-});
-
 app.get('/api/status', (req, res) => {
     return res.status(200).json({
         service: "sales-api",
@@ -47,8 +23,9 @@ app.get('/api/status', (req, res) => {
     });
 });
 
-
+app.use(express.json());
 app.use(checkToken);
+app.use(orderRoutes);
 
 app.listen(PORT, () => {
     console.info(`Server started successfully at port ${PORT}`);
